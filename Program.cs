5ec3c4d63e99d4e8b7a920c306fcd9982fad05b5
Seu Nome <thiago.pavan@edu.senai.br>
2026-04-15 15:49:43 -0300

    feat MENSAGEM DE DESCRIÇÃO DO PROJETO

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CalculadoraSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Calculadora Avançada SENAI ---");
+           
+            Console.Write("Digite o primeiro número: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Escolha a operação: (+, -, *, /)");
+            string operacao = Console.ReadLine();
+
+            double resultado = 0;
+
+            // Estrutura de decisão para selecionar a operação
+            switch (operacao)
+            {
+                case "+": resultado = num1 + num2; break;
+                case "-": resultado = num1 - num2; break;
+                case "*": resultado = num1 * num2; break;
+                case "/":
+                    // Verificação de segurança: divisão por zero
+                    resultado = num2 != 0 ? num1 / num2 : 0;
+                    if(num2 == 0) Console.WriteLine("Erro: Divisão por zero!");
+                    break;
+                default:
+                    Console.WriteLine("Operação inválida!");
+                    return;
+            }
+
+            Console.WriteLine($"O resultado de {num1} {operacao} {num2} é: {resultado}");
+        }
+    }
+}
